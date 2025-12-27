bb0c919f87a0a747c8baf274dfc236556fcff02f
umeshsolanki <umesh.solanki@go-mmt.com>
2025-12-27 21:08:50 +0530

    Add maven-publish plugin and configuration to shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinSerialization)
+    `maven-publish`
 }
 
 kotlin {
@@ -23,6 +24,21 @@ kotlin {
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+    }
+}
+
+group = "com.umeshsolanki.dockermanager"
+version = "1.0.0"
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://r1.umeshsolanki.in/repository/maven-releases/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME") ?: "public"
+                password = System.getenv("MAVEN_PASSWORD")?: "public"
+            }
         }
     }
 }
