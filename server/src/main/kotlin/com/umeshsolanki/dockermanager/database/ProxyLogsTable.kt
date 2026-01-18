407f3eafaa437bc8c931ea0252e34190a1a3ee4d
umeshsolanki <umesh.solanki@go-mmt.com>
2026-01-18 11:17:42 +0530

    Fix ProxyLogsTable method column length to support longer HTTP methods

--- server/src/main/kotlin/com/umeshsolanki/dockermanager/database/ProxyLogsTable.kt
+++ server/src/main/kotlin/com/umeshsolanki/dockermanager/database/ProxyLogsTable.kt
@@ -7,7 +7,7 @@ object ProxyLogsTable : Table("proxy_logs") {
     val id = long("id").autoIncrement()
     val timestamp = datetime("timestamp").index()
     val remoteIp = varchar("remote_ip", 45).index()
-    val method = varchar("method", 10)
+    val method = varchar("method", 20)
     val path = text("path")
     val status = integer("status").index()
     val responseTime = long("response_time").default(0)
